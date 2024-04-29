6a4ab9fd189691109438330b3033538e12fa0303
Mohamed Nejjar <109230561+MoNejjar@users.noreply.github.com>
2024-04-29 23:04:38 +0200

    Update generation.py - dummy method to make the UI work
    since the actual llm rag chain doesn't work yet (check TODO) i've implemented a simple retriever that gives back the content of the first document that matches the query

--- generation.py
+++ generation.py
@@ -38,13 +38,13 @@ class ChatBot:
     def answer_question(self, question, model, conversation):
         # prompt engineering to guide the LLM through the provided documents
         prompt_template = ("""
-You will be faced with queries concerning a couple of vehicle models. You
-are a salesperson and you should assist people using the information that is 
-provided to you while being as concise as possible. The people you're dealing
-are very important people and they don't have much time so the answer
-needs to be simple. If you don't know the answer, just say I don't know.
-Answer with a short sentence then ask the user if he wants more detail 
-on the car.
+                           You will be faced with queries concerning a couple of vehicle models. You
+                           are a salesperson and you should assist people using the information that is 
+                           provided to you while being as concise as possible. The people you're dealing
+                           are very important people and they don't have much time so the answer
+                           needs to be simple. If you don't know the answer, just say I don't know.
+                           Answer with a short sentence then ask the user if he wants more detail 
+                           on the car.
 """)
         prompt = ChatPromptTemplate.from_template(prompt_template)
 
@@ -56,12 +56,14 @@ on the car.
             openai_connected = True
 
         # create a rag chain instance if the api key is valid
-        if openai_connected:
-            rag_chain = (
+                if openai_connected:
+            """rag_chain = (
                 # TODO: turn the question into a Runnable instance that returns the query
                 {"context": retriever, "question": question}
                 | prompt
                 | model
                 | StrOutputParser()
             )
-            rag_chain.invoke(question)
+            rag_chain.invoke(question)"""
+            answer = retriever.invoke(question)[0].page_content
+            return answer
